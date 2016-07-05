52b0f2eba6ed5b93a946f49bc987f3ffe35eb3f3
Ryllez <makuta20@mail.ru>
2016-07-05 10:40:45 +0600

    initial project version

---
+++ Calcul/Calcul/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Double.Parse(textBox1.Text) + Double.Parse(textBox2.Text));
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Double.Parse(textBox1.Text) * Double.Parse(textBox2.Text));
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Double.Parse(textBox1.Text) - Double.Parse(textBox2.Text));
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = Convert.ToString(Double.Parse(textBox1.Text) / Double.Parse(textBox2.Text));
+        }
+    }
+}
